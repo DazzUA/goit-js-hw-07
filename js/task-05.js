c2a5ad05ffd4020e2892a725e00978d4b4e1ebe6
dazzUA <pavelbelozerov18@gmail.com>
2023-12-18 19:57:28 +0200

    finish

--- js/task-05.js
+++ js/task-05.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.querySelector(".change-color").addEventListener("click", function () {
+  let body = document.querySelector("body");
+  let colorSpan = document.querySelector(".color");
+  
+  let randomColor = getRandomHexColor();
+  
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
